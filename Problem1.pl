b90f35338f9baa91fd7a3f06d47fd798f83b3d22
aaronkauffman123 <aaron.r.kauffman@gmail.com>
2017-01-04 19:07:38 -0500

    completed problem 1 code

--- Problem1.pl
+++ Problem1.pl
@@ -3,4 +3,14 @@
 #1/4/17
 #https://projecteuler.net/problem=1
 
+#Description: Find the sum of all the multiples of 3 or 5 below 1000.
 
+$sum = 0;
+
+for($i = 0; $i < 1000; $i++){
+	if(($i % 3 == 0) || ($i % 5 == 0)){
+		$sum = $sum + $i;
+	} 
+}
+
+print "The sum is $sum\n";
